200ba33f7e58e475975ac1010a37a7542cd4c7b6
unknown <venuslove1704@gmail.com>
2025-05-09 18:06:01 -0700

    Modificaciones

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,31 @@
-s
+import { isValidElement, useState } from "react"
+import '.SearchBar.css'
+
+const SearchBar = ({ onSubmit }) => {
+    const [term,setTerm] = useState('')
+
+    const handleForSubmit = (event) => {
+        event.preventDefault()
+        console.log('Necesito decirle al componente papa sobre los datos')
+        onSubmit(term)
+    }
+
+    const handleChange = (event) => {
+        setTerm(event.target.value)
+    }
+
+    console.log('term', term)
+
+    return(
+        <div className="search-bar">
+            <form onSubmit={handleForSubmit}>
+                <label>Termino de la busqueda:</label>
+                <input onChange={handleChange} value={term}/>
+            </form>
+        </div>
+    )
+
+    
+}
+
+export default SearchBar
